ace633482b39de2d909bd76f8ba7eaa8f29ba3b2
Itgel Delgerdalai <itgeldelgerdalai12@gmail.com>
2021-06-24 14:23:33 +0800

    style7

--- src/screens/Home/Profile.js
+++ src/screens/Home/Profile.js
@@ -8,31 +8,22 @@ export class Profile extends React.Component {
     }
     render() {        
         return(
-            <div className="fluid-container" id="createProfile">
-                <div className="form-bg">
+            <div id="createProfile" className="container-fluid">
+                <div>
                     <div className="card text-end">
                         <i className="fa fa-times-circle"></i>
                     </div>
-                    <div className="card-group">
+                </div>
+                <div className="card-group row">
+                    <div className="col-sm-2">
                         <div className="card">
                             <img src="./logo192.png"/>
                         </div>
-                        <div className="card">
-                            <div className="card-item mt-4">
-                                <p><b>Username</b></p>
-                                <input type="text" required minLength="1"></input>
-                            </div>
-                            <div className="card-item mt-4">
-                                <p><b>Bio</b></p>
-                                <input type="text" required minLength="1"></input>
-                            </div>
-                            <div className="card-item mt-4">
-                                <p><b>Password</b></p>
-                                <input type="text" required minLength="1"></input>
-                            </div>
-                            <div className="card-item">
-                                <button className="btn btn-blue">Edit</button>
-                            </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-item">
+                            <p><b>Username</b></p>
+                            <input type="text" placeholder="New username"></input>
                         </div>
                     </div>
                 </div>
