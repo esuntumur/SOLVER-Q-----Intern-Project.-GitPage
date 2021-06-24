4616bba75ddba1039abdf6ed4680cb1bd64f3c47
Esoo <esontomorgantomor@gmail.com>
2021-06-24 13:26:56 +0800

    after merge

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -358,7 +358,7 @@ class Home extends Component {
                     questions.length > 0 &&
                     questions.map((i, idx) => (
                       <div key={idx}>
-                        <div className="card-group shadow p-5 m-5 border rounded">
+                        <div className="row card-group shadow p-5 m-5 border rounded">
                           <div className="col-sm-2">
                             <div className="card text-center">
                               <div className="card-body">
@@ -397,28 +397,28 @@ class Home extends Component {
                               </div>
                             </div>
                           </div>
-                        </div>
-                        <div className="col-sm-10">
-                          <div className="card">
-                            <div className="card-body">
-                              <h5
-                                className="card-title"
-                                onClick={() => this.props.setSelectedQuestion(i)}
-                              >
-                                <b>{i.title}</b>
-                              </h5>
-                              <div>
-                                <div
-                                  className="questionStyle card-text"
-                                  dangerouslySetInnerHTML={{
-                                    __html: i.question.substring(0, 300) + "...",
-                                  }}
-                                ></div>
+                          <div className="col-sm-10">
+                            <div className="card">
+                              <div className="card-body">
+                                <h5
+                                  className="card-title"
+                                  onClick={() => this.props.setSelectedQuestion(i)}
+                                >
+                                  <b>{i.title}</b>
+                                </h5>
+                                <div>
+                                  <div
+                                    className="questionStyle card-text"
+                                    dangerouslySetInnerHTML={{
+                                      __html: i.question.substring(0, 300) + "...",
+                                    }}
+                                  ></div>
+                                </div>
+                                <p className="card-text text-end">
+                                  <i>by </i>
+                                  <b> {i.user.username}</b>
+                                </p>
                               </div>
-                              <p className="card-text text-end">
-                                <i>by </i>
-                                <b> {i.user.username}</b>
-                              </p>
                             </div>
                           </div>
                         </div>
